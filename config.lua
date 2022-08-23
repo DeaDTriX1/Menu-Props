5b5f722052efd9fc7a507e98ff7ff6c9a7bc1810
DeaDTriX <97814611+DeaDTriX1@users.noreply.github.com>
2022-08-23 15:20:03 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,53 @@
+Config = {}
+
+-- AJOUTE TES PROPS ICI
+
+Config.Props = {
+    {nameprops = 'Chaise',              modelprops = 'apa_mp_h_din_chair_12'},
+    {nameprops = 'Carton',              modelprops = 'prop_cardbordbox_04a'},
+    {nameprops = 'Sac',                 modelprops = 'prop_cs_heist_bag_02'},
+    {nameprops = 'Table 1',             modelprops = 'prop_rub_table_02'},
+    {nameprops = 'Table 2',             modelprops = 'prop_table_04'},
+    {nameprops = 'Table 3',             modelprops = 'bkr_prop_weed_table_01b'},
+    {nameprops = 'Chaise 1',            modelprops = 'bkr_prop_clubhouse_chair_01'},
+    {nameprops = 'Chaise 2',            modelprops = 'bkr_prop_weed_chair_01a'},
+    {nameprops = 'Chaise de Pêche',     modelprops = 'hei_prop_hei_skid_chair'},
+    {nameprops = 'Chaise de Bureau',    modelprops = 'bkr_prop_clubhouse_offchair_01a'},
+    {nameprops = 'Canapé',              modelprops = 'v_tre_sofa_mess_c_s'},
+    {nameprops = 'Canapé 2',            modelprops = 'v_res_tre_sofa_mess_a'},
+    {nameprops = 'Ordinateur',          modelprops = 'bkr_prop_clubhouse_laptop_01a'},
+    {nameprops = 'Lit',                 modelprops = 'gr_prop_bunker_bed_01'},
+    {nameprops = 'Outils',              modelprops = 'prop_cs_trolley_01'},
+    {nameprops = 'Outils de mécanique', modelprops = 'prop_carcreeper'},
+    {nameprops = 'Sac de sport',        modelprops = 'prop_cs_heist_bag_02'},
+    {nameprops = 'Trousse médical 2',   modelprops = 'xm_prop_x17_bag_med_01a'},
+}
+
+
+
+-- Active ou non Le menu pour tout le monde
+
+ActiveMenu = true
+
+
+
+
+
+-- Active ou non la restriction a un groupe ( superadmin, mod ,ect )
+
+Restrein = false
+
+
+
+
+
+-- le nom des group peuvent etre totalement changé !
+
+Group = {
+    Authorize1 = 'user',
+    --Authorize2 = 'mod',
+    --Authorize3 = 'admin',
+    --Authorize4 = 'superadmin',
+    --Authorize5 = '_dev',
+}
+
